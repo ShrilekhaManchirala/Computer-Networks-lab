efc55864bdef3aeb7fbc990e5572d55cc496f8c0
ShrilekhaManchirala <manchiralashrilekha2004@gmail.com>
2024-10-30 14:00:17 +0530

    SUBNET.C

--- SUBNET.C
+++ SUBNET.C
@@ -1,66 +1,57 @@
 #include<stdio.h>
-int p,q,u,v,n;
-int min=99,mincost=0;int t[50][2],i,j;
-int parent[50],edge[50][50];
-int find(int);
-void sunion(int,int);
-void main()
-{
-clrscr();
-printf("\n Enter the number of nodes");scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-printf("%c\t",65+i);parent[i]=-1;
+#include<stdlib.h>
+#define MAX_HOSTS 100
+#define QUEUE_SIZE 100
+int queue[QUEUE_SIZE];
+int front=0,rear=0;
+void enqueue(int v){
+if(rear==QUEUE_SIZE){
+printf("Queue overflow\n");
+return;
 }
-printf("\n");
-for(i=0;i<n;i++)
-{
-printf("%c",65+i);
-for(j=0;j<n;j++)
-scanf("%d",&edge[i][j]);
+queue[rear++]=v;
 }
-for(i=0;i<n;i++)
-{
-for(j=0;j<n;j++)
-if(edge[i][j]!=99)
-if(min>edge[i][j])
-{
-min=edge[i][j];
-u=i;
-v=j;
+int dequeue(){
+if(front==rear){
+printf("Queue underflow\n");
+return -1;
 }
-p=find(u);
-q=find(v);
-if(p!=q)
-{
-t[i][0]=u;
-t[i][1]=v;
-mincost=mincost+edge[u][v];
-sunion(p,q);
+return queue[front++];
 }
-else
-{
-t[i][0]=-1;
-t[i][1]=-1;
+int isEmpty() {
+return front==rear;
 }
-min=99;
-}
-printf("Minimum cost is %d\n Minimum spanning tree is\n",mincost);
-for(i=0;i<n;i++)
-if(t[i][0]!=-1&&t[i][1]!=-1)
+void bfs(int adjMatrix[MAX_HOSTS][MAX_HOSTS],int numHosts,int start){
+int visited[MAX_HOSTS]={0};
+int i;
+enqueue(start);
+visited[start]=1;
+printf("Broadcast Tree(BFS order):\n");
+while(!isEmpty()){
+int currentHost=dequeue();
+printf("Host %d\n",currentHost);
+for(i=0;i<numHosts;i++){
+if(adjMatrix[currentHost][i]==1&&!visited[i])
 {
-printf("%c %c %d",65+t[i][0],65+t[i][1],
-edge[t[i][0]][t[i][1]]);
-printf("\n");
+enqueue(i);
+visited[i]=1;
 }
 }
-void sunion(int l,int m)
-{
-parent[l]=m;
 }
-find(int l)
-{
-if(parent[l]>0)
-l=parent[l];
-return l;
+}
+int main(){
+int numHosts,i,j,startHost;
+int adjMatrix[MAX_HOSTS][MAX_HOSTS];
+printf("Enter the number of hosts in the subnet:");
+scanf("%d",&numHosts);
+printf("Enter the adjecency matrix(0 for no connection,1 for connection):\n");
+for(i=0;i<numHosts;i++){
+for(j=0;j<numHosts;j++){
+scanf("%d",&adjMatrix[i][j]);
+}
+}
+printf("Enter the starting host for the broadcast (0 to %d):",numHosts-1);
+scanf("%d",&startHost);
+bfs(adjMatrix,numHosts,startHost);
+return 0;
 }
